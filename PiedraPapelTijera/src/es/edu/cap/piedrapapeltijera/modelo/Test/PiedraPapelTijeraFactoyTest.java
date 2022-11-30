c4067567c0e2a891d7f4142f8ed319b8a5d871f1
Trujillo Rey <viviana.trujillo-rey@capgemini.com>
2022-11-30 10:41:17 +0100

    add more test

--- PiedraPapelTijera/src/es/edu/cap/piedrapapeltijera/modelo/Test/PiedraPapelTijeraFactoyTest.java
+++ PiedraPapelTijera/src/es/edu/cap/piedrapapeltijera/modelo/Test/PiedraPapelTijeraFactoyTest.java
@@ -36,12 +36,63 @@ class PiedraPapelTijeraFactoyTest {
 		assertTrue(PiedraPapelTijeraFactoy.getInstance(PiedraPapelTijeraFactoy.PIEDRA) instanceof Piedra);
 	}
 	@Test
+	void test_getInstance_Piedra_false() {
+		assertFalse(PiedraPapelTijeraFactoy.getInstance(PiedraPapelTijeraFactoy.PIEDRA) instanceof Papel);
+	}
+	@Test
 	void test_getInstance_Papel() {
 		assertTrue(PiedraPapelTijeraFactoy.getInstance(PiedraPapelTijeraFactoy.PAPEL) instanceof Papel);
+	}
+	@Test
+	void test_getInstance_Papel_false() {
+		assertFalse(PiedraPapelTijeraFactoy.getInstance(PiedraPapelTijeraFactoy.PAPEL) instanceof Tijera);
 	}
 	@Test
 	void test_getInstance_Tijera() {
 		assertTrue(PiedraPapelTijeraFactoy.getInstance(PiedraPapelTijeraFactoy.TIJERA) instanceof Tijera);
 	}
+	@Test
+	void test_getInstance_Tijera_false() {
+		assertFalse(PiedraPapelTijeraFactoy.getInstance(PiedraPapelTijeraFactoy.TIJERA) instanceof Papel);
+	}
+	@Test
+	void test_Piedra_gana_Tijera() {
+		assertEquals(1, piedra.comparar(tijera));
+	}
+	@Test
+	void test_Tijera_pierde_Piedra() {
+		assertEquals(-1, tijera.comparar(piedra));		
+	}
+	@Test
+	void test_Tijera_gana_Papel() {
+		assertEquals(1, tijera.comparar(papel));
+	}
+	@Test
+	void test_Papel_pierde_Piedra() {
+		assertEquals(-1, papel.comparar(tijera));		
+	}
+	@Test
+	void test_Papel_gana_Piedra() {
+		assertEquals(1, papel.comparar(piedra));
+	}
+	@Test
+	void test_Piedra_pierde_Papel() {
+		assertEquals(-1, piedra.comparar(papel));		
+	}
+	@Test
+	void test_Piedra_igual_Piedra() {
+		assertEquals(0, piedra.comparar(piedra));
+	}
+	@Test
+	void test_Papel_igual_Papel() {
+		assertEquals(0, papel.comparar(papel));
+	}
+	@Test
+	void test_Tijera_igual_Tijera() {
+		assertEquals(0, tijera.comparar(tijera));
+	}
+	
+	
+	
 
 }
